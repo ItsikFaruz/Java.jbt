1ccfb7fbfe81a4b9aa44a55eec35f35f2ca98d94
a <a@a-PC>
2021-12-17 12:19:50 +0200

    Merge branch 'master' of https://github.com/ItsikFaruz/Java.jbt

--- lab_exercise/src/lab_6/ex_2.java
+++ lab_exercise/src/lab_6/ex_2.java
@@ -1,20 +1,24 @@
 package lab_6;
 
-public class ex_1 {
+public class ex_2 {
 
 	public static void main(String[] args) {
 		int[] arr = new int[39];
 		arr[0] = 1;
 		arr[1] = 1;
-
-		System.out.print(arr[0] + "," + arr[1]);
+		
+		int fbRand = (int)(Math.random()*41);
+		
+		
 
 		for (int i = 2; i < arr.length; i++) {
 			arr[i] = arr[i - 1] + arr[i - 2];
-			System.out.print("," + arr[i]);
-		}
 	
+		}
 		
+		System.out.println(fbRand);
+		System.out.println(arr[fbRand]);
+			
 	
 	}	
 
