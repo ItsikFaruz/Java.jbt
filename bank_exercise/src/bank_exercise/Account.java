16dfbf5ef613d24a058cc3b74cd7f93925c3a547
a <a@a-PC>
2021-12-20 23:10:15 +0200

    aaa

---
+++ bank_exercise/src/bank_exercise/Account.java
@@ -0,0 +1,33 @@
+package bank_exercise;
+
+public class Account {
+
+		int id;
+		float balance;
+	
+		
+		public Account(int id, float balance) {
+			super();
+			this.id = id;
+			this.balance = balance;
+		}
+
+
+		public float getBalance() {
+			return balance;
+		}
+
+
+		public void setBalance(float balance) {
+			this.balance = balance;
+		}
+
+
+		public int getId() {
+			return id;
+		}
+		
+		
+		
+		
+}
